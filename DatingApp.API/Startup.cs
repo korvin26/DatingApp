50436d24d23484ecd347d77208f852866fa6f8f8
Mark Lipovetski <markl@DEV1.LOCAL>
2020-04-12 21:58:04 +0300

    Bad migration

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -34,6 +34,7 @@ namespace DatingApp.API
             //     options.SuppressAsyncSuffixInActionNames = false)
             //     .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
